2d08bfb41bf314ad2b1ee49df0422f5c9dcc78ab
Fabio Lopes <nunes-lopes@hotmail.com>
2025-06-12 17:40:58 -0300

    configuração php tela registro de contato

--- config/process.php
+++ config/process.php
@@ -5,12 +5,33 @@ session_start();
 include_once("connection.php");
 include_once("url.php");
 
-$contacts = [];
+$id = null;
 
-$query = "SELECT * FROM contacts";
+if (!empty($_GET)) {
+    $id = $_GET["id"];
+}
 
-$stmt = $conn->prepare($query);
+// Retorna os dados de um contato
+if (!empty($id)) {
 
-$stmt->execute();
+    $query = "SELECT * FROM contacts WHERE id=:id";
 
-$contacts = $stmt->fetchAll();
+    $stmt = $conn->prepare($query);
+
+    $stmt->bindParam(":id", $id);
+
+    $stmt->execute();
+
+    $contact = $stmt->fetch();
+} else {
+    // Retorna todos os contatos
+    $contacts = [];
+
+    $query = "SELECT * FROM contacts";
+
+    $stmt = $conn->prepare($query);
+
+    $stmt->execute();
+
+    $contacts = $stmt->fetchAll();
+}
